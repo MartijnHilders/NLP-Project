83c3911b216d67ab51bb7dec8a60b4fe1c88553f
MartijnHilders <martijnhilders@live.nl>
2019-05-23 12:52:12 +0200

    vo

--- Main.py
+++ Main.py
@@ -1,7 +1,30 @@
+import warnings
+warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
+
+import gensim
+import nltk
+import string
+import Data
+import numpy
+from nltk.corpus import stopwords
+from nltk import word_tokenize, sent_tokenize
+from gensim.models import Word2Vec
+
+
 def main():
-    """ Main program """
-    # Code goes over here.
+    cleanAndTokenize()
     return 0
 
+
+
+
+
+def cleanAndTokenize():
+    text = 1
+    print("hello")
+    cleaned_text = text
+
+    return cleaned_text
+
 if __name__ == "__main__":
-    main()
+        main()
