7936edb96b48420d18f23f1d8ecb6a3e224494f4
Casteeuwen <35225641+Casteeuwen@users.noreply.github.com>
2019-05-25 14:08:12 +0200

    Added unigram model

--- Main.py
+++ Main.py
@@ -1,23 +1,29 @@
 import warnings
 warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
-
 import gensim
 import nltk
 import string
 import Data
 import numpy as np
+import collections
 import re
 import pandas as pd
 from nltk.corpus import stopwords
 from nltk import word_tokenize, sent_tokenize
+from nltk.tokenize import wordpunct_tokenize
 from string import punctuation
+import math
 from gensim.models import Word2Vec
 
 
 
 def main():
 
+    print(wordpunct_tokenize("Cool. Yes, very cool!"))
+
     # importing the training data using pandas.
+    # nltk.download('stopwords')
+    # nltk.download('punkt')
     df_train = pd.read_csv('Data/train.csv')
 
     # save the original text for convenience
@@ -25,12 +31,9 @@ def main():
 
     # save the cleaned text for convenience
     # df_train['comment_text'].apply(cleanText).to_csv('Data/cleanedText.csv')
-
+    trainingAlgorithm(df_train)
     df_train['comment_text'] = df_train['comment_text'].apply(cleanText)
-    print(df_train)
-
-
-
+    print(df_train['comment_text'])
     return 0
 
 
@@ -64,6 +67,8 @@ def cleanText(text, stem_words=True):
     text = re.sub("were\'nt", " were not ", text, flags=re.IGNORECASE)
     text = re.sub("can't", " can not ", text, flags=re.IGNORECASE)
     text = re.sub("\'ve", " have ", text)
+    text = re.sub("\'ll", " will ", text)
+
 
     #remove all the stopwords
     stopword = word_tokenize(text)
@@ -78,17 +83,58 @@ def cleanText(text, stem_words=True):
         return ''
 
 
-    cleaned_text = text
 
+    cleaned_text = text
+    #print(cleaned_text)
     return cleaned_text
 
-def trainingAlgorithm():
-
-
+def trainingAlgorithm(dataset):
     #TODO: get the cleaned training data through the algorithm  trainign it with the accompanied labels
+    print('Started training')
+
+    #unigram model
+    toxicUnigramCounts = collections.defaultdict(lambda: 0)
+    toxicTotal = 0
+    neutralUnigramCounts = collections.defaultdict(lambda: 0)
+    neutralTotal = 0
+
+    for index, row in dataset.iterrows():
+        #print(row['id'])
+        for word in word_tokenize(row['comment_text']):
+            if row['toxic']==1:
+                toxicUnigramCounts[word] = toxicUnigramCounts[word] + 1
+                toxicTotal += 1
+            if row['toxic']==0:
+                neutralUnigramCounts[word] = neutralUnigramCounts[word] + 1
+                neutralTotal += 1
+    print(toxicTotal)
 
 
     return 0
+
+def unigramtesting(sentence,toxicTotal,neutralTotal,toxicUnigramCounts,neutralUnigramCounts):
+
+
+    toxicScore = 0.0
+    neutralScore = 0.0
+    for token in sentence:
+        toxicCount = toxicUnigramCounts[token] + 1
+        toxicScore += math.log(toxicCount)
+        toxicScore -= math.log(toxicTotal)
+
+        neutralCount = neutralUnigramCounts[token] + 1
+        toxicScore += math.log(neutralCount)
+        toxicScore -= math.log(neutralTotal)
+
+    if toxicScore > neutralScore:
+        return 1
+    else:
+        return 0
+
+
+
+
+
 
 
 if __name__ == "__main__":
